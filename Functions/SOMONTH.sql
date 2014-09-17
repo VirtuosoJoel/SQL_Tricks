70cea6c4f0507462a80ebb27693e98f0deede82f
Joel Pearson <virtuosojoel@gmail.com>
2014-09-17 09:30:15 +0100

    Create SOMONTH.sql

---
+++ Functions/SOMONTH.sql
@@ -0,0 +1,21 @@
+IF OBJECT_ID('dbo.SOMONTH',N'FN') IS NOT NULL
+  DROP FUNCTION dbo.SOMONTH
+GO
+
+CREATE FUNCTION SOMONTH
+(
+  @Date as DATETIME
+)
+RETURNS DATETIME
+BEGIN
+
+  RETURN dateadd(month, datediff(month, 0, ISNULL(@Date,getdate())),0)
+
+END
+GO
+
+select dbo.SOMONTH(NULL) as 'NULL_ARG'
+GO
+
+select dbo.SOMONTH(getdate()) as 'GETDATE_ARG'
+GO

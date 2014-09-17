e9f01ef9f94227071a2987f3b72dc3d573539683
Joel Pearson <virtuosojoel@gmail.com>
2014-09-17 09:45:56 +0100

    Update BASE64STRING.sql

--- Functions/BASE64STRING.sql
+++ Functions/BASE64STRING.sql
@@ -14,3 +14,13 @@ BEGIN
 	RETURN CAST(N'' AS XML).value('xs:base64Binary(xs:hexBinary(sql:variable("@conversion")))', 'VARCHAR(MAX)')
 
 END
+
+/*
+
+This is designed to take a gif which was saved as an image in the database and turn it into a "src" arribute in html
+"The_Base_64_String" section of the below javascript can be overwritten with the base64 string, thus displaying an image.
+
+
+javascript:window.open('data:text/html;charset=utf-8,' +encodeURIComponent('<html><body><div><img src="data:image/gif;base64,The_Base_64_String" /></div></body></html>'));
+
+*/
